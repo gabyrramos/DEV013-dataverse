c5984bf716e836ffdb116a32c52a5e56ffcb4801
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-01-12 11:46:12 -0500

    Agregando contenido
    Creamos funciones

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+//Llamamos funciones desde view 
+
+//Creamos una variable para obtener el contenido de p
+var contenedor= document.getElementById('contenedor'); 
+//Llamamos a la funcion
+crearParrafo(contenedor);
+
 console.log(example, renderItems(data), data);
