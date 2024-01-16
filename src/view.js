8f6277bb0da3443397b9b58f266611bf8eb7a4cc
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-01-16 11:49:23 -0500

    OH
    CAMBIOS OH

--- src/view.js
+++ src/view.js
@@ -1,5 +1,19 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
+     // Obtén el contenedor por su ID
+     const contenedorParrafo = document.createElement('div');
+  
+     // Crear un elemento p (párrafo)
+    const nuevoParrafo = document.createElement('p');
+     
+    // Agregar texto al nuevo párrafo con datos dinámicos
+    nuevoParrafo.textContent = 'Epopeya';
+  
+     // Agregar el párrafo al contenedor
+    contenedorParrafo.appendChild(nuevoParrafo);
+   
+    // Retornar el contenedor actualizado
+    return contenedorParrafo;
   return 'example';
 };
