c572d4dfe4470a9148637c8275f564fd6c155979
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-01-16 11:50:34 -0500

    OH
    CAMBIOS OH

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
+var contenedor = document.getElementById('contenedor');
+contenedor.innerHTML=renderItems;
 
 console.log(example, renderItems(data), data);
