c2242d03fded8ba0644cc147560dd8b1f153ecdc
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-01-23 19:29:57 -0500

    Manipulacion del DOM

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -10,6 +10,41 @@ export const anotherExample = () => {
   return [];
 };
 
+export const ordenarNombresAZ = (data) => {
+  // Ordenar el array de objetos 'data' por el nombre en orden alfabético (A-Z)
+  const dataOrdenada = data.slice().sort((a, b) => {
+    const nombreA = a.name.toUpperCase();
+    const nombreB = b.name.toUpperCase();
+
+    if (nombreA < nombreB) {
+      return -1;
+    }
+    if (nombreA > nombreB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return dataOrdenada;
+};
+
+export const ordenarNombresZA = (data) => {
+  // Ordenar el array de objetos 'data' por el nombre en orden alfabético (A-Z)
+    const dataOrdenada = data.slice().sort((a, b) => {
+    const nombreA = a.name.toUpperCase();
+    const nombreB = b.name.toUpperCase();
+
+    if (nombreA > nombreB) {
+      return -1;
+    }
+    if (nombreA < nombreB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return dataOrdenada;
+};
 //una funcion para filtrar
 //parametros:data, filterby, como quiero filtrar(facts)
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 //import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
+import {ordenarNombresAZ, ordenarNombresZA} from './dataFunctions.js';
+
 
 import data from './data/datamovies.js';
 import { example } from './dataFunctions.js';
@@ -9,6 +11,29 @@ import { example } from './dataFunctions.js';
 const showr = renderItems(data);
 const contenedor = document.getElementById('contenedor');
 contenedor.innerHTML = showr;
+
+
+const selectSort = document.querySelector('[data-testid="select-sort"]');
+
+// Función para actualizar la vista cuando se cambian los selectores
+const actualizarVista = () => {
+    let datosOrdenados = data; // Inicialmente, usa los datos sin ordenar
+  
+    // Verifica la opción seleccionada en el selector de orden
+    if (selectSort.value === 'asc') {
+      datosOrdenados = ordenarNombresAZ(datosOrdenados);
+     } else if (selectSort.value === 'desc') {
+       datosOrdenados = ordenarNombresZA(datosOrdenados);
+    }
+  
+    // Llama a la función renderItems y actualiza el contenido del contenedor
+    const showo = renderItems(datosOrdenados);
+    contenedor.innerHTML = showo;
+  };
+  
+  // Agrega eventos de cambio a los selectores
+ 
+  selectSort.addEventListener('change', actualizarVista);
 
 
 
