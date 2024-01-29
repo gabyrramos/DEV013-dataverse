e32fd1ff5b119a2adeca3512eef7c26292ccd655
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-01-29 09:23:57 -0500

    Cambios no sirven

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -14,7 +14,7 @@ export const anotherExample = () => {
 
 export const ordenarNombresAZ = (data) => {
   // Ordenar el array de objetos 'data' por el nombre en orden alfabético (A-Z)
-  const dataOrdenada = data.slice().sort((a, b) => {
+    const dataOrdenada = data.slice().sort((a, b) => {
     const nombreA = a.name.toUpperCase();
     const nombreB = b.name.toUpperCase();
 
@@ -32,7 +32,7 @@ export const ordenarNombresAZ = (data) => {
 
 export const ordenarNombresZA = (data) => {
   // Ordenar el array de objetos 'data' por el nombre en orden alfabético (A-Z)
-  const dataOrdenada = data.slice().sort((a, b) => {
+    const dataOrdenada = data.slice().sort((a, b) => {
     const nombreA = a.name.toUpperCase();
     const nombreB = b.name.toUpperCase();
 
@@ -53,5 +53,6 @@ export const ordenarNombresZA = (data) => {
 export const generoMovies = (data,categoriaSeleccionada) => {
   // Filtra las películas por el género seleccionado
   const genero = data.filter(movie=>movie.genero===categoriaSeleccionada)
+  console.log(genero)
   return genero; 
 };
