ae7d6388774f984b61dff8ed7a41e6285ed941ec
Gabriela R Ramos <gabrielarramos@GabrielaRRamos.local>
2024-02-12 00:50:00 -0500

    actualizando tests

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,17 +1,103 @@
-import { example, anotherExample } from '../src/dataFunctions.js';
+//import { example, anotherExample } from '../src/dataFunctions.js';
 
+import { ordenarNombresAZ, ordenarNombresZA, generoMovies, premiosGanadosTotal, promedioPremiosGanados } from '../src/dataFunctions.js';
 
+describe('ordenarNombresAZ', () => {
+  it('debería ordenar correctamente un array de objetos por el campo "name" en orden alfabético ascendente', () => {
+    const data = [
+      { name: 'Simon' },
+      { name: 'Joaquin' },
+      { name: 'Adriana' },
+    ];
+    const resultadoEsperado = [
+      { name: 'Adriana' },
+      { name: 'Joaquin' },
+      { name: 'Simon' },
+    ];
+    expect(ordenarNombresAZ(data)).toEqual(resultadoEsperado);
+  });
 
-describe('example', () => {
+  it('debería mantener el orden si el array ya está ordenado alfabéticamente', () => {
+    const data = [
+      { name: 'Adriana' },
+      { name: 'Joaquin' },
+      { name: 'Simon' },
+    ];
+    expect(ordenarNombresAZ(data)).toEqual(data);
+  });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('debería devolver un array vacío si se proporciona un array vacío', () => {
+    expect(ordenarNombresAZ([])).toEqual([]);
+  });
+
+});
+
+describe('ordenarNombresZA', () => {
+  it('debería ordenar correctamente un array de objetos por el campo "name" en orden alfabético ascendente', () => {
+    const data = [
+      { name: 'Simon' },
+      { name: 'Joaquin' },
+      { name: 'Adriana' },
+    ];
+    const resultadoEsperado = [
+      { name: 'Simon' },
+      { name: 'Joaquin' },
+      { name: 'Adriana' },
+    ];
+    expect(ordenarNombresZA(data)).toEqual(resultadoEsperado);
+  });
+
+  it('debería mantener el orden si el array ya está ordenado alfabéticamente', () => {
+    const data = [
+      { name: 'Simon' },
+      { name: 'Joaquin' },
+      { name: 'Adriana' },
+    ];
+    expect(ordenarNombresZA(data)).toEqual(data);
+  });
+
+  it('debería devolver un array vacío si se proporciona un array vacío', () => {
+    expect(ordenarNombresZA([])).toEqual([]);
   });
 });
 
-describe('anotherExample', () => {
+describe('generoMovies', () => {
+  const data = [
+    { genero: 'Romance' },
+    { genero: 'Action' },
+  ];
+  const categoriaSeleccionada = 'Romance';
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('deberia retornar el genero correcto', () => {
+    const resultado = generoMovies(data, categoriaSeleccionada);
+    expect(resultado).toEqual([{ genero: 'Romance' }]);
+  });
+});
+
+
+describe('premiosGanadosTotal', () => {
+  const data = [
+    { facts: { premioGanado: 1 } },
+    { facts: { premioGanado: 2 } },
+  ];
+
+  it('Deberia sumar todos los premios ganados de los 24 objetos', () => {
+    const sumatoria = premiosGanadosTotal(data);
+    expect (sumatoria).toBe(3);
+  });
+});
+
+describe('promedioPremiosGanados', () => {
+  const data = [
+    { facts: { premioGanado: 1 } },
+    { facts: { premioGanado: 2 } },
+  ];
+  
+  it('Deberia dar el promedio de la suma total entre los 24 objetos', () =>{
+    const sumatoria = premiosGanadosTotal(data);
+    const datos = promedioPremiosGanados(data);
+
+    const promedio = data.length >0 ? sumatoria/data.length : 0;
+    expect (datos).toEqual(promedio);
   });
 });
